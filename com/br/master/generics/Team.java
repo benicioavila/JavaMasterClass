438395d8336308d539fb8279e4b227ea5d793230
Benicio Brito <benicio.brito@sepog.fortaleza.ce.gov.br>
2025-01-22 15:01:56 -0300

    example class generic

---
+++ com/br/master/generics/Team.java
@@ -0,0 +1,26 @@
+package com.br.master.generics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Team <T> {
+    private String teamName;
+    private List<T> teamMembers = new ArrayList();
+
+    private int totalWins = 0;
+
+    private int totalLosses = 0;
+
+    private int totalTies = 0;
+
+    public Team (String teamName){
+
+    }
+
+    public void addTeam(T t){
+        if(!teamMembers.contains(t)){
+            teamMembers.add(t);
+        }
+    }
+
+}
