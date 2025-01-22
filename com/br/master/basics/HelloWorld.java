8bc8130e3ebe62bd9d2a70ff79f52083c69932ac
Benicio Brito <benicio.brito@sepog.fortaleza.ce.gov.br>
2025-01-22 10:54:44 -0300

    first commit

---
+++ com/br/master/basics/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.br.master.basics;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+    
+}
---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $org.masterclass;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
