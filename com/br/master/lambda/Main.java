9ea716b10afbce5c336102b3fa859dc1e50c7b6e
Benicio Brito <benicio.brito@sepog.fortaleza.ce.gov.br>
2025-02-02 11:11:20 -0300

    anonymus class and lambda expression

--- com/br/master/lambda/Main.java
+++ com/br/master/lambda/Main.java
@@ -1,4 +1,47 @@
 package com.br.master.lambda;
 
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+
 public class Main {
+
+    record Person (String firstName, String lastName){
+
+        @Override
+        public String toString(){
+            return firstName + " " + lastName;
+        }
+    }
+
+    public static void main(String[] args) {
+        List<Person> people = new ArrayList<>(Arrays.asList(
+                new Main.Person("Benicio", "Avila"),
+                new Person("Maria", "Feitosa")
+        ));
+
+        //Using anonymou class
+        var comparatorLasName = new Comparator<Person>(){
+                public int compare(Person o1, Person o2){
+                    return o1.lastName().compareTo(o2.lastName);
+                }
+        };
+
+        people.sort(comparatorLasName);
+
+        people.sort( new Comparator<Person>(){
+            public int compare(Person o1, Person o2){
+                return o1.lastName().compareTo(o2.lastName);
+            }
+        });
+
+        people.sort((o1, o2) -> o1.lastName().compareTo(o2.lastName));
+
+        System.out.println(people);
+
+    }
 }
+
